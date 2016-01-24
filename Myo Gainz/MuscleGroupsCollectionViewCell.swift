24f4cecc79600edcaf716fad01749a82759bb69c
Domingo <Domingo@inside-203-177.wireless.utoronto.ca>
2016-01-24 05:29:20 -0500

    GAINZZZ

--- Myo Gainz/MuscleGroupsCollectionViewCell.swift
+++ Myo Gainz/MuscleGroupsCollectionViewCell.swift
@@ -11,12 +11,5 @@ import UIKit
 class MuscleGroupsCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        imageView = UIImageView(frame: frame)
-//    }
-//
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
+
 }
